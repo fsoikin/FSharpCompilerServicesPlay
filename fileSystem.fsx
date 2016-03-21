b01fe4395f092990b4f69d375df66a1603287804
Fyodor Soikin <name.fa@gmail.com>
2016-03-21 14:07:05 -0400

    Initial commit.

---
+++ fileSystem.fsx
@@ -0,0 +1,36 @@
+﻿open System.IO
+open System
+open Microsoft.FSharp.Compiler.AbstractIL.Internal.Library
+
+let makeFileSystem (getFile: string -> byte[] option) = 
+  let time = DateTime.Now
+  let ni() = failwith "not implemented"
+  { new IFileSystem with
+    member x.AssemblyLoad n = ni()
+    member x.AssemblyLoadFrom n = ni()
+    member x.FileDelete n = ni()
+    member x.FileStreamCreateShim n = ni()
+             
+    member x.FileStreamReadShim fileName = 
+      match getFile fileName with
+      | Some contents -> new MemoryStream( contents ) :> Stream
+      | None -> null
+             
+    member x.FileStreamWriteExistingShim n = ni()
+    member x.GetFullPathShim fileName = fileName
+    member x.GetLastWriteTimeShim n = time
+    member x.GetTempPathShim() = System.IO.Path.GetTempPath()
+    member x.IsInvalidPathShim n = false
+    member x.IsPathRootedShim n = false
+
+    member x.ReadAllBytesShim(fileName: string): byte [] = 
+      match getFile fileName with
+      | Some contents -> contents
+      | None -> 
+        try
+          File.ReadAllBytes fileName
+        with _ -> null
+             
+    member x.SafeExists fileName = 
+      (getFile fileName |> Option.isSome) || (File.Exists fileName)
+  }
---
+++ run.fsx
@@ -0,0 +1,65 @@
+﻿#r "./packages/FSharp.Compiler.Service/lib/net45/FSharp.Compiler.Service.dll"
+#load "fileSystem.fsx"
+
+open System
+open Microsoft.FSharp.Compiler.AbstractIL.Internal.Library
+open Microsoft.FSharp.Compiler.SourceCodeServices
+open FileSystem
+
+let checker = FSharpChecker.Create(keepAssemblyContents=true)
+let opts = {
+  FSharpProjectOptions.IsIncompleteTypeCheckEnvironment = false
+  ProjectFileName = "project.fsproj"
+  ProjectFileNames = [|"file.fs"|]
+  OtherOptions = [|"--out:x.dll"; "--target:library"|]
+  ReferencedProjects = [||]
+  UseScriptResolutionRules = false
+  LoadTime = DateTime.Now
+  UnresolvedReferences = None }
+
+let srcBytes = 
+  """
+      module X
+    
+      let f x y = x+y
+      let g = f 1
+      let h = (g 2) + 3
+  """ 
+  |> System.Text.Encoding.UTF8.GetBytes
+
+Shim.FileSystem <- makeFileSystem (fun fn -> if fn = "file.fs" then Some srcBytes else None)
+
+let res = checker.ParseAndCheckProject( opts ) |> Async.RunSynchronously
+let ast = res.AssemblyContents.ImplementationFiles
+
+(*
+val ast : FSharpImplementationFileContents list =
+  [Microsoft.FSharp.Compiler.SourceCodeServices.FSharpImplementationFileContents
+     {Declarations = [Entity
+                        (X,
+
+      [MemberOrFunctionOrValue
+        (val f,[[val x]; [val y]],
+        Call
+          (null,val op_Addition,[],
+           [type Microsoft.FSharp.Core.int; type Microsoft.FSharp.Core.int;
+            type Microsoft.FSharp.Core.int],[Value val x; Value val y]));
+
+       MemberOrFunctionOrValue
+        (val g,[],
+        Let
+          ((val x, Const (1,type Microsoft.FSharp.Core.int)),
+           Lambda (val y,Call (null,val f,[],[],[Value val x; Value val y]))));
+
+       MemberOrFunctionOrValue
+        (val h,[],
+        Application
+          (Call (null,val g,[],[],[]),[],[Const (2,type Microsoft.FSharp.Core.int)]))
+        // ^^^^^ no call to (+) and (Const 3) is missing!
+      ])];
+
+      FileName = "file.fs";
+      HasExplicitEntryPoint = false;
+      IsScript = false;
+      QualifiedName = "X";}]
+*)
